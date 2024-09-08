39c7cdaaea909e970b93b9b7ca7f32a5746cb4cf
Amandot <amanmprajapati@gmail.com>
2024-09-08 11:41:34 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+ "use strict";//treat all js code
+
+//  alert(3+3) uing node js not browser
+
+let name = "aman"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bright 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol =>unique 
+
+//object 
+
+console.log(typeof "aman")//string
+console.log(typeof age)//obj
+console.log(typeof null)
