dcb4c31048c7a6dd369b045d63bdb124d8afaec7
Amandot <amanmprajapati@gmail.com>
2024-09-08 12:09:21 +0000

    conversion of data types

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,12 @@
+let score = true
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//aman => true
+//"" => false
